28b885eea5416f3ffa0c5bb8618df8c3bc1b3b97
Johan Östby <johan.gotet@gmail.com>
2021-01-14 18:08:49 +0100

    Lagt till best seller-produkter på startsidan

--- wp-content/themes/labb4/front-page.php
+++ wp-content/themes/labb4/front-page.php
@@ -2,13 +2,20 @@
 <div class="row">
 
 <div class="col-lg-12">
+
+<h1 class="text-center">Best Sellers</h1>
+<p class="text-center">Brilliant design and unparalleled craftsmanship.</p>
+<?php
+echo do_shortcode('[best_selling_products columns="4" per_page="8"]');
+?>
+
 <h1 class="text-center">Shop by Category</h1>
 <p class="text-center">Brilliant design and unparalleled craftsmanship.</p>
 <?php
 echo do_shortcode('[product_categories columns="6" number="6"]');
 ?>
-</div>
 
+</div>
 </div>
 </div>
 
