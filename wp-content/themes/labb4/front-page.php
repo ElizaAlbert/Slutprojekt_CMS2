6e74fa79a75d7d9bb851d111c812c28757914abf
Emil Backlund <emilbacklund@outlook.com>
2021-01-18 14:49:40 +0100

    basic styling of puff banner news

--- wp-content/themes/labb4/front-page.php
+++ wp-content/themes/labb4/front-page.php
@@ -1,39 +1,45 @@
 <?php get_header(); ?>
+</div> <!-- end container from header -->
 
-<div class="row puff">
-  <div class="col-lg-3">
-    <h2>Recent News</h2>
-  </div>
-  <div class="col-lg-9">
-    <?php $the_query = new WP_Query('posts_per_page=1'); ?>
-    <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-    <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-    <?php the_excerpt(__('(more…)')); ?>
-    <?php
-endwhile;
-wp_reset_postdata();
-?>
+<div class="puff mt-5 mb-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3">
+        <h2>Recent News</h2>
+      </div>
+      <div class="col-lg-9">
+        <?php $the_query = new WP_Query('posts_per_page=1'); ?>
+        <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
+        <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+        <?php the_excerpt(__('(more…)')); ?>
+        <?php
+      endwhile;
+      wp_reset_postdata();
+      ?>
+      </div>
+    </div>
   </div>
 </div>
+<div class="container">
+  <!-- start container from header -->
+  <div class="row">
 
-<div class="row">
+    <div class="col-lg-12">
 
-  <div class="col-lg-12">
-
-    <h1 class="text-center">Best Sellers</h1>
-    <p class="text-center">Brilliant design and unparalleled craftsmanship.</p>
-    <?php
+      <h1 class="text-center">Best Sellers</h1>
+      <p class="text-center">Brilliant design and unparalleled craftsmanship.</p>
+      <?php
 echo do_shortcode('[best_selling_products columns="4" per_page="8"]');
 ?>
 
-    <h1 class="text-center">Shop by Category</h1>
-    <p class="text-center">Brilliant design and unparalleled craftsmanship.</p>
-    <?php
+      <h1 class="text-center">Shop by Category</h1>
+      <p class="text-center">Brilliant design and unparalleled craftsmanship.</p>
+      <?php
 echo do_shortcode('[product_categories columns="6" number="6"]');
 ?>
 
+    </div>
   </div>
-</div>
 </div>
 
 <div class="banner"
