d6feedb8cbd29489b2c190c6296a8aff43fac535
Emil Backlund <emilbacklund@outlook.com>
2021-01-20 10:28:17 +0100

    Removed unused file

--- wp-content/themes/labb4/checkoutfix.js
+++
@@ -1,4 +0,0 @@
-$('#customer_details > .col-1').removeClass('col-1').addClass('col-6').css({"display": "inline-block", "vertical-align": "top"});
-$('#customer_details > .col-2').removeClass('col-2').addClass('col-5').css('display','inline-block');
-$('.woocommerce-MyAccount-content .col-1').removeClass('col-1').addClass('col-12');
-$('.woocommerce-MyAccount-content .col-2').removeClass('col-2').addClass('col-12');
