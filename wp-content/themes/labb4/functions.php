67e787af336e135743630e68e4e2d9b66c1aa70e
Johan Östby <johan.gotet@gmail.com>
2021-01-21 11:27:21 +0100

    Lagt till Google fonts och mobilanpassat delar

--- wp-content/themes/labb4/functions.php
+++ wp-content/themes/labb4/functions.php
@@ -4,6 +4,7 @@
       wp_enqueue_style('style.css', get_stylesheet_uri());
       wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css', array(), '5.10.2');
       wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', array(), '4.5.2');
+      wp_enqueue_style('poppins-font', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap', array(), '1.0.0' );
       wp_enqueue_script('jquery');
       wp_enqueue_script('bootstrapjs', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'), '4.5.2', true);
       //   wp_enqueue_script('checkoutfix', get_template_directory_uri() .'/checkoutfix.js', array('jquery'), '1', true);
