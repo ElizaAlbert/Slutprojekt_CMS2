c8b8541e7215c010abcaa0e823a0d11ebe45a156
Emil Backlund <emilbacklund@outlook.com>
2021-01-21 15:27:34 +0100

    added jQuery cdn to funcrions

--- wp-content/themes/labb4/functions.php
+++ wp-content/themes/labb4/functions.php
@@ -4,12 +4,18 @@
       wp_enqueue_style('style.css', get_stylesheet_uri());
       wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css', array(), '5.10.2');
       wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', array(), '4.5.2');
-      wp_enqueue_style('poppins-font', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap', array(), '1.0.0' );
-      wp_enqueue_script('jquery');
+      wp_enqueue_style('poppins-font', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap', array(), '1.0.0');
       wp_enqueue_script('bootstrapjs', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array('jquery'), '4.5.2', true);
       //   wp_enqueue_script('checkoutfix', get_template_directory_uri() .'/checkoutfix.js', array('jquery'), '1', true);
   }
   add_action('wp_enqueue_scripts', 'slutprojekt_styles');
+
+  function include_jquery()
+  {
+      wp_deregister_script('jquery');
+      wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.5.1.min.js', array(), null, false);
+  }
+add_action('wp_enqueue_scripts', 'jquery');
 
 function slutprojekt_setup()
 {
@@ -27,7 +33,8 @@ function slutprojekt_menus()
 }
 add_action('init', 'slutprojekt_menus');
 
-function slutprojekt_custom_excerpt($excerpt) {
+function slutprojekt_custom_excerpt($excerpt)
+{
     if (has_excerpt()) {
         $excerpt = wp_trim_words(get_the_excerpt(), apply_filters("excerpt_length", 30));
     }
